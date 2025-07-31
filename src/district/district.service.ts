b807a76337d6713e024431c9d2117cc878208ec3
madina875 <evaevasmq@gmail.com>
2025-07-31 18:22:41 +0500

    with districtfull

--- src/district/district.service.ts
+++ src/district/district.service.ts
@@ -1,26 +1,65 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { CreateDistrictDto } from './dto/create-district.dto';
 import { UpdateDistrictDto } from './dto/update-district.dto';
+import { PrismaService } from '../prisma/prisma.service';
 
 @Injectable()
 export class DistrictService {
+  constructor(private readonly prismaService: PrismaService) {}
+
   create(createDistrictDto: CreateDistrictDto) {
-    return 'This action adds a new district';
+    return this.prismaService.district.create({
+      data: createDistrictDto,
+    });
   }
 
   findAll() {
-    return `This action returns all district`;
+    return this.prismaService.district.findMany({
+      include: {
+        region: true,
+      },
+    });
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} district`;
+  async findOne(id: number) {
+    const district = await this.prismaService.district.findUnique({
+      where: { id },
+      include: { region: true },
+    });
+
+    if (!district) {
+      throw new NotFoundException(`District with ID ${id} not found`);
+    }
+
+    return district;
   }
 
-  update(id: number, updateDistrictDto: UpdateDistrictDto) {
-    return `This action updates a #${id} district`;
+  async update(id: number, updateDistrictDto: UpdateDistrictDto) {
+    const district = await this.prismaService.district.findUnique({
+      where: { id },
+    });
+
+    if (!district) {
+      throw new NotFoundException(`District with ID ${id} not found`);
+    }
+
+    return this.prismaService.district.update({
+      where: { id },
+      data: updateDistrictDto,
+    });
   }
 
-  remove(id: number) {
-    return `This action removes a #${id} district`;
+  async remove(id: number) {
+    const district = await this.prismaService.district.findUnique({
+      where: { id },
+    });
+
+    if (!district) {
+      throw new NotFoundException(`District with ID ${id} not found`);
+    }
+
+    return this.prismaService.district.delete({
+      where: { id },
+    });
   }
 }
