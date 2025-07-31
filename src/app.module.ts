650ee4d84120481232930a364b74599003514677
madina875 <evaevasmq@gmail.com>
2025-07-31 18:18:24 +0500

    with district

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,9 +4,10 @@ import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
 import { RegionModule } from './region/region.module';
 import { PrismaModule } from './prisma/prisma.module';
+import { DistrictModule } from './district/district.module';
 
 @Module({
-  imports: [ConfigModule.forRoot({ envFilePath: '.env', isGlobal: true }), RegionModule, PrismaModule],
+  imports: [ConfigModule.forRoot({ envFilePath: '.env', isGlobal: true }), RegionModule, PrismaModule, DistrictModule],
   controllers: [AppController],
   providers: [AppService],
 })
