bbe6647085256191611f71702d0754ab374eda60
madina875 <evaevasmq@gmail.com>
2025-08-01 22:52:55 +0500

    readmefull3

--- src/region/region.module.ts
+++ src/region/region.module.ts
@@ -2,10 +2,11 @@ import { Module } from '@nestjs/common';
 import { RegionService } from './region.service';
 import { RegionController } from './region.controller';
 import { PrismaModule } from '../prisma/prisma.module';
+import { JwtService } from '@nestjs/jwt';
 
 @Module({
   imports: [PrismaModule],
   controllers: [RegionController],
-  providers: [RegionService],
+  providers: [RegionService, JwtService],
 })
 export class RegionModule {}
